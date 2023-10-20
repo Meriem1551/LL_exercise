815d94827b338ff72fd6bd6ac5bc2b8f4d66eb30
Meriem1551 <meriem200409@gmail.com>
2023-10-20 12:19:43 +0200

    Creating func that calc average of a student

---
+++ calcAvrg.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * calc_average - calculates the average
+ * @head: pointer to the marks lists
+ * Return: average of marks
+ */
+
+float calc_average(Marks *head)
+{
+    float avgr = 0, sum_marks = 0, sum_coef = 0;
+    Marks *ptr = head;
+
+    while (ptr != NULL)
+    {
+        sum_marks += (ptr->mark) * (ptr->coef);
+        sum_coef += ptr->coef;
+    }
+    avgr = (float)sum_marks / sum_coef;
+    return avgr;
+}

ac6035558b8fb1f7218a5019db8b9203a547a012
Meriem1551 <meriem200409@gmail.com>
2023-10-20 11:27:40 +0200

    Creating mark list

---
+++ createMark.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_mark_list - func create a list of marks
+ * @n: number of nodes
+ * Return: a pointer
+ */
+Marks *create_mark_list(int n)
+{
+    int i = 0, coef;
+    float mark;
+    Marks *p_m = (Marks *)malloc(sizeof(Marks)), *head, *ptr;
+    if (p_m == NULL)
+    {
+        printf("Allocation failed");
+        return NULL;
+    }
+    p_m->mark = getFloat("Enter the mark");
+    p_m->coef = getInt("Enter its coefficient");
+    p_m->next_note = NULL;
+    head = p_m;
+    ptr = head;
+    while (i < n - 1)
+    {
+        p_m = (Marks *)malloc(sizeof(Marks));
+        p_m->mark = getFloat("Enter the mark");
+        p_m->coef = getInt("Enter its coefficient");
+        p_m->next_note = NULL;
+        ptr->next_note = p_m;
+        ptr = ptr->next_note;
+    }
+    free(p_m, ptr);
+    return head;
+}

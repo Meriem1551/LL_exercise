bef57b0c5eed7958ada0bcfead7e439df602d0eb
Meriem1551 <meriem200409@gmail.com>
2023-10-20 12:28:49 +0200

    Creating func which scan an int

---
+++ getInt.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * getInt - reads input from user and returns it
+ * @s: the message will be displayed to the user
+ * Return: integer
+ */
+
+int getInt(char *s)
+{
+    int input;
+    puts(s);
+    scanf("%d", &input);
+    return input;
+}
